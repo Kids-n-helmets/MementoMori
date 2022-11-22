7a344fcb16513c8050e4ec7ecf37b484394890b1
Kids-n-helmets <113114491+Kids-n-helmets@users.noreply.github.com>
2022-11-22 11:00:04 +0000

    Added new trees and foot prints

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -10,19 +10,32 @@ public class CharacterController : MonoBehaviour
     public float rotation = 0.0f;
     public float camRotation = 0.0f;
     GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject  groundChecker;
+    public LayerMask groundLayer;
+    public float jumpforce= 300.0f;
 
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+       isOnGround = Physics.CheckSphere(groundChecker.transform.position,0.1f,groundLayer);
 
+       if (isOnGround==true && Input.GetKeyDown(KeyCode.Space))
+       {
+         myRigidbody.AddForce(transform.up * jumpforce);
+       }
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
